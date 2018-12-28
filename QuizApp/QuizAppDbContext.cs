dbc76a367111ba72bd0be3a18dd4e7a96c7fa776
Jacek Noga <jaceknoga11@gmail.com>
2018-12-28 17:29:28 +0100

    Added Question repository interface.

---
+++ QuizApp/QuizAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using QuizApp.Entities;
+
+namespace QuizApp
+{
+    public class QuizAppDbContext : DbContext
+    {
+        public QuizAppDbContext() : base("name=QuizAppConnectionString")
+        {
+            
+        }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
---
+++ QuizApp/Repositories/Abstract/IQuestionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizApp.Entities;
+
+namespace QuizApp.Repositories.Abstract
+{
+    public interface IQuestionRepository
+    {
+        IEnumerable<Question> GetAll();
+        Question Get(int id);
+        void Insert(Question question);
+        void Edit(Question question);
+        void Remove(int id);
+    }
+}
