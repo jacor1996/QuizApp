c701443dc4a7c8472a8684a9a263ded24c1295ad
Jacek Noga <jaceknoga11@gmail.com>
2018-12-28 18:12:24 +0100

    Added Questions controller.

---
+++ QuizApp/Controllers/QuestionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuizApp.Repositories.Abstract;
+
+namespace QuizApp.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private IQuestionRepository _questionRepository;
+
+        public QuestionsController(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        // GET: Questions
+        public ActionResult Index()
+        {
+            var data = _questionRepository.GetAll();
+            return View(data);
+        }
+    }
+}
